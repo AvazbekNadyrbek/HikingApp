d22e3250e216b6d0126185432c566b69188c30c3
AvazbekNadyrbek <spatium.avazbek@gmail.com>
2026-05-11 16:40:29 +0200

    Added a YAML Properties

--- src/main/java/com/berchtesgaden/explorer/exception/UserAlreadyExistsException.java
+++ src/main/java/com/berchtesgaden/explorer/exception/UserAlreadyExistsException.java
@@ -2,6 +2,6 @@ package com.berchtesgaden.explorer.exception;
 
 public class UserAlreadyExistsException extends RuntimeException {
     public UserAlreadyExistsException(String field, String value) {
-        super(field + "alredy exists: " + value);
+        super(field + "already exists: " + value);
     }
 }
